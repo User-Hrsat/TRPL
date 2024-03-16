93daa323dd99380712807e9ef255245cc594137d
User-Hrsat <38604393+User-Hrsat@users.noreply.github.com>
2024-03-16 14:38:49 +0800

    synchronize local code

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faild to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        println!("Your guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Tool small!"),
+            Ordering::Greater => println!("Tool big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                println!("Input quit to exit.");
+
+                let mut exit = String::new();
+
+                io::stdin()
+                    .read_line(&mut exit)
+                    .expect("Faild to read line");
+
+                break;
+            }
+        }
+    }
+}
